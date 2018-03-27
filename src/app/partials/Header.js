1102988105da09c3abc52158e1d1a2f48bd6d41d
dejan-krstic <dejanmkrstic@gmail.com>
2018-03-27 23:15:41 +0200

    semantic rules

--- src/app/partials/Header.js
+++ src/app/partials/Header.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import {Menu} from 'semantic-ui-react'      
+import { Menu } from 'semantic-ui-react'
 
-const items = [
-    { key: 'feed', active: false, name: 'Feed' },
-    { key: 'people', name: 'People' },
-    { key: 'profile', name: 'Profile' },
-]
 
-const Header = () => {
-    return (<Menu>
+const Header = (props) => {   
+    return (
+        <Menu borderless>
             <Menu.Menu position='right'>
-                <Menu.Item active={true} onClick={console.log}>Feed</Menu.Item>
-                <Menu.Item onClick={console.log}>People</Menu.Item>
-                <Menu.Item onClick={console.log}>Profile</Menu.Item>
+            <Menu.Item><h1>Bitbook</h1></Menu.Item>
             </Menu.Menu>
-            </Menu>
+            <Menu.Menu position='right'>
+                <Menu.Item active={window.location.pathname === '/'}  href='/'>Feed</Menu.Item>
+                <Menu.Item active={window.location.pathname === 'people'}  href='#/people'>People</Menu.Item>
+                <Menu.Item active={window.location.pathname === 'profile'}  href='#/profile'>Profile</Menu.Item>
+            </Menu.Menu>
+        </Menu>
     )
 }
 
